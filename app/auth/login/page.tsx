29f273f196e590cac7b3ca8b622c9a3167e318c5
Diego Ramirez <diego.ramirez2d03@gmail.com>
2026-02-23 12:58:16 -0600

    feat: estructura de directorios y primeras páginas

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,9 @@
+import { titleFont } from "../../ui/fonts";
+
+export default function Home() {
+  return (
+    <section>
+      <h1 className={`${titleFont.className} font-bold`}>Login</h1>
+    </section>
+  );
+}
---
+++ app/auth/new-account/page.tsx
@@ -0,0 +1,9 @@
+import { titleFont } from "../../ui/fonts";
+
+export default function Home() {
+  return (
+    <section>
+      <h1 className={`${titleFont.className} font-bold`}>New Account</h1>
+    </section>
+  );
+}
