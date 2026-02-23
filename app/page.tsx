a00e2dfef8d725cf082396ca173a205dcff67e97
Diego Ramirez <diego.ramirez2d03@gmail.com>
2026-02-23 12:33:48 -0600

    style:  Next.JS Fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import { titleFont } from "./ui/fonts";
 
 export default function Home() {
   return (
     <div>
       <h1>E-COMMERCE NEXT-JS</h1>
+      <h1 className={`${titleFont.className} font-bold`}>E-COMMERCE NEXT-JS</h1>
     </div>
   );
 }
