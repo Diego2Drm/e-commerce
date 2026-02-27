a29070107769df7c290c630fd7a2308d5018605a
Diego Ramirez <diego.ramirez2d03@gmail.com>
2026-02-26 20:20:00 -0600

    feat:  menú lateral | Segunda parte

--- app/components/products/product-grid/ProductGrid.tsx
+++ app/components/products/product-grid/ProductGrid.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export const ProductGrid = ({ products }: Props) => {
   return (
-    <section className="grid grid-cols-1 sm:grid-cols-2 gap-10 pb-10">
+    <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 pb-10">
       {
         products.map(product => (
           <ProductGridItem key={product.slug} product={product} />
--- app/components/sidebar/index.tsx
+++ app/components/sidebar/index.tsx
@@ -1,5 +1,6 @@
 'use client';
-import { IoCloseOutline, IoSearchOutline } from "react-icons/io5"
+import Link from "next/link";
+import { IoCloseOutline, IoLogInOutline, IoLogOutOutline, IoPeopleOutline, IoPersonOutline, IoSearchOutline, IoShirtOutline, IoTicketOutline } from "react-icons/io5"
 
 export const Sidebar = () => {
 
@@ -20,23 +21,85 @@ export const Sidebar = () => {
       <aside className="fixed p-5 right-0 top-0 w-[500px] h-screen bg-white z-20 shadow-2xl transform transition-all duration-300">
 
         <IoCloseOutline
-          size={50}
-          className="absolute right-5 top-5 cursor-pointer text-black"
+          size={30}
+          className="absolute right-5 top-4 cursor-pointer text-black"
           onClick={() => console.log('click')}
         />
 
         {/* Input */}
-        <div className="relative mt-14">
+        <div className="relative mt-10">
           <IoSearchOutline
-          size={20}
-          className="text-black absolute top-2 left-2"
+            size={20}
+            className="text-black absolute top-2 left-2"
           />
           <input type="text"
-          placeholder="Buscar"
-          className="w-full bg-gray-100 rounded pl-10 py-1 pr-10 border-b-2 text-xl border-gray-400 focus:outline-none focus:border-blue-500 placeholder:text-black placeholder:opacity-30"
+            placeholder="Buscar"
+            className="w-full bg-gray-100 rounded pl-10 py-1 pr-10 border-b-2 text-xl border-gray-400 focus:outline-none focus:border-blue-500 placeholder:text-black placeholder:opacity-30"
           />
-
         </div>
+
+        {/* Menu */}
+        <Link
+          href="/"
+          className="flex items-center mt-6 p-2 hover:bg-gray-100 rounded transition-all"
+        >
+          <IoPersonOutline size={30} className="text-black" />
+          <span className="ml-3 text-xl text-black">Perfil</span>
+        </Link>
+
+        <Link
+          href="/"
+          className="flex items-center mt-6 p-2 hover:bg-gray-100 rounded transition-all"
+        >
+          <IoTicketOutline size={30} className="text-black" />
+          <span className="ml-3 text-xl text-black">Ordenes</span>
+        </Link>
+
+        <Link
+          href="/"
+          className="flex items-center mt-6 p-2 hover:bg-gray-100 rounded transition-all"
+        >
+          <IoLogInOutline size={30} className="text-black" />
+          <span className="ml-3 text-xl text-black">Ingresar</span>
+        </Link>
+
+        <Link
+          href="/"
+          className="flex items-center mt-6 p-2 hover:bg-gray-100 rounded transition-all"
+        >
+          <IoLogOutOutline size={30} className="text-black" />
+          <span className="ml-3 text-xl text-black">Salir</span>
+        </Link>
+
+        {/* Line Separator */}
+        <div className="w-full h-px bg-gray-300 my-10" />
+
+        <Link
+          href="/"
+          className="flex items-center mt-6 p-2 hover:bg-gray-100 rounded transition-all"
+        >
+          <IoShirtOutline size={30} className="text-black" />
+          <span className="ml-3 text-xl text-black">Productos</span>
+        </Link>
+
+        <Link
+          href="/"
+          className="flex items-center mt-6 p-2 hover:bg-gray-100 rounded transition-all"
+        >
+          <IoTicketOutline size={30} className="text-black" />
+          <span className="ml-3 text-xl text-black">Ordenes</span>
+        </Link>
+
+        <Link
+          href="/"
+          className="flex items-center mt-6 p-2 hover:bg-gray-100 rounded transition-all"
+        >
+          <IoPeopleOutline size={30} className="text-black" />
+          <span className="ml-3 text-xl text-black">Usuarios</span>
+        </Link>
+
+
+
       </aside>
     </section>
 
