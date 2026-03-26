251994f8877c264bdacb08ee2abd60831b5d3786
Diego Ramirez <diego.ramirez2d03@gmail.com>
2026-03-25 18:12:46 -0600

    feat: pantalla de producto

--- app/(shop)/product/[slug]/page.tsx
+++ app/(shop)/product/[slug]/page.tsx
@@ -1,7 +1,52 @@
-export default function () {
+import { initialData } from "@/app/seed/seed";
+import { titleFont } from "@/app/ui/fonts";
+import { notFound } from "next/navigation";
+
+interface Props {
+  params: {
+    slug: string
+  }
+}
+
+export default async function ({ params }: Props) {
+  const { slug } = await params;
+  const product = initialData.products.find(product => product.slug === slug);
+
+  if (!product) notFound();
+
+
   return (
-    <div>
-      Product Page
-    </div>
+    <section className="mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3">
+      {/* Slides Show */}
+      <div className="col-span-1 md:col-span-2">
+        {slug}
+      </div>
+
+      {/* Detalles */}
+      <div className="col-span-1 px-5">
+
+        <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>
+          {product.title}
+        </h1>
+        <p className="text-lg mb-5">
+          ${product.price}
+        </p>
+        {/* Selector de Tallas */}
+
+        {/* Selector de Cantidad */}
+
+
+        {/* Button */}
+        <button className="btn-primary mb-5">
+          Agregar al Carrito
+        </button>
+
+        {/* Descripcion */}
+        <h3 className="font-bold text-sm">
+          Descripción
+        </h3>
+        <p>{product.description}</p>
+      </div>
+    </section>
   )
 }
---
+++ app/(shop)/product/not-found.tsx
@@ -0,0 +1,8 @@
+import PageNotFound from "@/app/components/ui/not-found/PageNotFound";
+
+export default function () {
+
+  return (
+    <PageNotFound />
+  )
+}
